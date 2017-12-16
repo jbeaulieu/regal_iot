42a20ba4299227af423c7228d1c70f1cbdea1597
Jon Beaulieu <jon@jonbeaulieu.com>
2017-12-15 18:11:41 -0600

    Toggle bluetooth discovery
    Added button to toggle bluetooth discovery for 10 seconds. Button will be removed after testing automatic discovery of gateway device

--- android/RegalIoT/app/src/main/java/com/regal/iotdemo/GatewayBluetoothActivity.java
+++ android/RegalIoT/app/src/main/java/com/regal/iotdemo/GatewayBluetoothActivity.java
@@ -1,7 +1,6 @@
 package com.regal.iotdemo;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,15 +15,15 @@ import android.widget.Button;
 public class GatewayBluetoothActivity extends AppCompatActivity {
 
     private static final String TAG = "BluetoothActivity";
-    BluetoothAdapter mBlueToothAdapter;
+    BluetoothAdapter mBluetoothAdapter;
 
-    // Create a BroadcastReceiver for ACTION_STATE_CHANGED.
-    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+    // Broadcast Receiver for changes to BT action state (on/off)
+    private final BroadcastReceiver mActionStateBroadcastReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            // When the bluetooth receiver changes action state
-            if (action.equals(mBlueToothAdapter.ACTION_STATE_CHANGED)) {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBlueToothAdapter.ERROR);
+
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
 
                 switch (state) {
                     case BluetoothAdapter.STATE_ON:
@@ -44,44 +43,92 @@ public class GatewayBluetoothActivity extends AppCompatActivity {
         }
     };
 
+    // Broadcast Receiver for changes to BT Discovery states (on/off/expired)
+    private final BroadcastReceiver mDiscoveryBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if(action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
+                int mode = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
+
+                switch (mode) {
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
+                        Log.d(TAG, "mDiscoveryBroadcastReceiver: Discoverability Enabled");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
+                        Log.d(TAG, "mDiscoveryBroadcastReceiver: Discoverability Disabled. Able to receive connections from paired devices only");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_NONE:
+                        Log.d(TAG, "mDiscoveryBroadcastReceiver: Discoverability Disabled. Unable to receive connections");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTING:
+                        Log.d(TAG, "mDiscoveryBroadcastReceiver: Connecting...");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTED:
+                        Log.d(TAG, "mDiscoveryBroadcastReceiver: Connected");
+                        break;
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gateway_bluetooth);
         setupActionBar();
 
-        Button mBluetoothButton = (Button) findViewById(R.id.bluetooth_toggle);
+        Button mBluetoothToggleButton = (Button) findViewById(R.id.bluetooth_on_off_toggle);
+        Button mBluetoothDiscoveryButton = (Button) findViewById(R.id.bluetooth_discovery_toggle);
 
-        mBlueToothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        mBluetoothButton.setOnClickListener(new View.OnClickListener() {
+        mBluetoothToggleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: Toggling BT");
                 ToggleBluetooth();
             }
         });
+
+        mBluetoothDiscoveryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Toggling Discoverability");
+                ToggleDiscovery();
+            }
+        });
     }
 
     public void ToggleBluetooth() {
-        if(mBlueToothAdapter == null) {
+        if(mBluetoothAdapter == null) {
             Log.d(TAG, "ToggleBluetooth: Device does not support BT");
         }
-        else if(!mBlueToothAdapter.isEnabled()) {   // BT is disabled, turn on
+        else if(!mBluetoothAdapter.isEnabled()) {   // BT is disabled, turn on
             Log.d(TAG, "ToggleBluetooth: Turning on BT");
             Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivity(enableBTIntent);
-            // Register new IntentFilter to catch BT state changes - changes will flag mBroadcastReceiver
+            // Register new IntentFilter to catch BT state changes - changes will flag mActionStateBroadcastReceiver
             IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mBroadcastReceiver, BTIntent);
+            registerReceiver(mActionStateBroadcastReceiver, BTIntent);
         }
-        else if(mBlueToothAdapter.isEnabled()) {    // BT is already enabled, turn off
+        else if(mBluetoothAdapter.isEnabled()) {    // BT is already enabled, turn off
             Log.d(TAG, "ToggleBluetooth: Turning off BT");
-            mBlueToothAdapter.disable();
-            // Register new IntentFilter to catch BT state changes - changes will flag mBroadcastReceiver
+            mBluetoothAdapter.disable();
+            // Register new IntentFilter to catch BT state changes - changes will flag mActionStateBroadcastReceiver
             IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mBroadcastReceiver, BTIntent);
+            registerReceiver(mActionStateBroadcastReceiver, BTIntent);
         }
+    }
+
+    public void ToggleDiscovery() {
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 10);
+        startActivity(discoverableIntent);
+
+        IntentFilter intentFilter = new IntentFilter(mBluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        registerReceiver(mDiscoveryBroadcastReceiver, intentFilter);
     }
 
     // Unregister broadcast receiver when application is paused or destroyed.
@@ -89,7 +136,7 @@ public class GatewayBluetoothActivity extends AppCompatActivity {
     protected void onDestroy() {
         Log.d(TAG, "Calling onDestroy()");
         super.onDestroy();
-        unregisterReceiver(mBroadcastReceiver);
+        unregisterReceiver(mActionStateBroadcastReceiver);
     }
 
     private void setupActionBar() {
